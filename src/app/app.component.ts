00b4ca0a4962349da4528f4b97907567f2e5b363
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-12-11 09:16:57 +0300

    Added a goal class and created instances of the goal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
+import {Goal} from './goal'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-	goals: string[];
+	goals = [
+		new Goal(1,'Watch Finding Nemo'),
+		new Goal(2,'Buy Cookies'),
+		new Goal(3, 'Get new Phone Case'),
+		new Goal(4, 'Get Dog Food'),
+		new Goal(5, 'Solve math homework'),
+		new Goal(6, 'Plot my world domination plan'),
 
-  constructor(){
-
-  	this.goals = ["Watch Finding Nemo","Buy Cookies","Get new phone case"];
-  }
+	]
 }
---
+++ src/app/goal.ts
@@ -0,0 +1,3 @@
+export class Goal {
+	constructor(public id:number, public name:string){}
+}
