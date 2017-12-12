5fcd3a63240f0aacb3c2f2afe5fa5b2db50c41c8
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-12-12 09:19:08 +0300

    Using Built in pipes

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,6 +1,6 @@
 export class Goal {
 	public showDescription:boolean
-	constructor(public id:number, public name:string,public description:string){
+	constructor(public id:number, public name:string,public description:string,public completeDate:Date){
 		this.showDescription=false
 
 	}
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -11,14 +11,15 @@ import {StrikethroughDirective} from '../strikethrough.directive'
 export class GoalComponent implements OnInit {
 
 	status:string='None';
+	// April 14 2018 January = 0 
 
 	goals = [
-		new Goal(1,'Watch Finding Nemo','Find an online version and watch merlin find his son'),
-		new Goal(2,'Buy Cookies','I have to buy cookies for the parrot'),
-		new Goal(3, 'Get new Phone Case','Diana has her birthday coming up soon'),
-		new Goal(4, 'Get Dog Food','Pupper likes expensive sancks'),
-		new Goal(5, 'Solve math homework','Damn Math'),
-		new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord'),
+		new Goal(1,'Watch Finding Nemo','Find an online version and watch merlin find his son',new Date(2018,3,14) ),
+		new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2018,6,9) ),
+		new Goal(3, 'Get new Phone Case','Diana has her birthday coming up soon',new Date(2018,1,12) ),
+		new Goal(4, 'Get Dog Food','Pupper likes expensive sancks',new Date(2018,0,18) ),
+		new Goal(5, 'Solve math homework','Damn Math',new Date(2018,2,14) ),
+		new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord',new Date(2018,3,14) ),
 
 	]
 
