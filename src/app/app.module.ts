0e83e4ecdf710d2b9622d67663e3c1db2e36ac8c
James Muriuki <jamesmuriukimbuthia@gmail.com>
2017-12-30 08:19:52 +0300

    Import FormsModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
     GoalFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
