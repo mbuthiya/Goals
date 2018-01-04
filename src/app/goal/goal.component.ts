917e700a0f4becb54b7248b670e1ef91c6ffec6c
James Muriuki <jamesmuriukimbuthia@gmail.com>
2018-01-04 09:59:22 +0300

    Moved the goals array to a separate folder

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 
 import {Goal} from '../goal'
+import {Goals} from '../goals'
 import {StrikethroughDirective} from '../strikethrough.directive'
 
 @Component({
@@ -13,16 +14,7 @@ export class GoalComponent implements OnInit {
 	status:string='None';
 	// April 14 2018 January = 0 
 
-	goals = [
-		new Goal(1,'Watch Finding Nemo','Find an online version and watch merlin find his son',new Date(2018,3,14) ),
-		new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2018,6,9) ),
-		new Goal(3, 'Get new Phone Case','Diana has her birthday coming up soon',new Date(2018,1,12) ),
-		new Goal(4, 'Get Dog Food','Pupper likes expensive sancks',new Date(2018,0,18) ),
-		new Goal(5, 'Solve math homework','Damn Math',new Date(2018,2,14) ),
-		new Goal(6, 'Plot my world domination plan','Cause I am an evil overlord',new Date(2018,3,14) ),
-
-	]
-
+	goals = Goals;
 
 	toogleDetails(index){
 		this.goals[index].showDescription = !this.goals[index].showDescription;
