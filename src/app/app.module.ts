3b38062ad7816d42ee5e3d1e7c8dee63d38f19cf
James Muriuki <jamesmuriukimbuthia@gmail.com>
2018-01-05 11:43:55 +0300

    Imported the http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
 import {AlertsService} from './alert-service/alerts.service'
 
@@ -24,7 +25,8 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [AlertsService],
   bootstrap: [AppComponent]
